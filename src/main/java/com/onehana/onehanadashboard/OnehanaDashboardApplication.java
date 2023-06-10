5ca1927b80f64cf629a8a1c742941a439ae1415d
kdongk <ldh5081@naver.com>
2023-06-10 20:14:10 +0900

    add: swagger 암호화 추가

--- src/main/java/com/onehana/onehanadashboard/OnehanaDashboardApplication.java
+++ src/main/java/com/onehana/onehanadashboard/OnehanaDashboardApplication.java
@@ -2,6 +2,8 @@ package com.onehana.onehanadashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class OnehanaDashboardApplication {
@@ -10,4 +12,8 @@ public class OnehanaDashboardApplication {
         SpringApplication.run(OnehanaDashboardApplication.class, args);
     }
 
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
--- src/main/java/com/onehana/onehanadashboard/config/SecurityConfig.java
+++ src/main/java/com/onehana/onehanadashboard/config/SecurityConfig.java
@@ -1,34 +1,69 @@
 package com.onehana.onehanadashboard.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.springframework.http.HttpMethod.*;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig implements WebMvcConfigurer {
 
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    @Value("${one-hana.swagger-id}")
+    private String swaggerId;
+
+    @Value("${one-hana.swagger-pd}")
+    private String swaggerPd;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        BasicAuthenticationEntryPoint basicAuthenticationEntryPoint = new BasicAuthenticationEntryPoint();
+        basicAuthenticationEntryPoint.setRealmName("swagger");
+
         return http
                 .cors()
                 .and()
-
                 .csrf().disable()
                 .formLogin().disable()
-                .httpBasic().disable()
-
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/swagger-ui/**").authenticated()
                         .anyRequest().permitAll()
                 )
-
+                .httpBasic(httpBasic -> httpBasic
+                        .authenticationEntryPoint(basicAuthenticationEntryPoint)
+                )
                 .build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        List<UserDetails> userDetailsList = new ArrayList<>();
+
+        userDetailsList.add(User.builder()
+                .username(swaggerId)
+                .password(passwordEncoder.encode(swaggerPd))
+                .roles("ADMIN")
+                .build());
+
+        return new InMemoryUserDetailsManager(userDetailsList);
     }
 
     @Override
